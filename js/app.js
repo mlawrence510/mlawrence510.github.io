4c2d4afd4bcb99524439e0ab97dfc07357242bfa
mlawrence510 <66187887+mlawrence510@users.noreply.github.com>
2020-08-12 10:02:18 -0400

    added onclick to overlay area

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
+const sidebar = document.querySelector("#mySidebar");
+const body = document.querySelector("body");
+const overlay = document.querySelector('.overlay');
+
+
+
+
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.querySelector("body").className = 'modal-open';
-  document.querySelector('.overlay').style.display = 'block';
+  sidebar.style.width = "250px";
+  body.className = 'modal-open';
+  overlay.style.display = 'block';
 }
 
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+
 function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.querySelector("body").className = "";
-  document.querySelector('.overlay').style.display = 'none';
+  sidebar.style.width = "0";
+  body.className = "";
+  overlay.style.display = 'none';
 }
